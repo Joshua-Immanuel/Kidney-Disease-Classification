d0307d91476d05f77e5463bdf1fbf45c269f31fb
Joshua-Immanuel <vemana20620@iiitkalyani.ac.in>
2024-02-08 13:03:33 +0545

    save model added

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -48,3 +48,20 @@ class PrepareBaseModel:
 
         full_model.summary()
         return full_model
+
+        
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
