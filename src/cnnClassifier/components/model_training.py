36160990a810ddcd2e47bff4a85ed75a28de309f
Joshua-Immanuel <vemana20620@iiitkalyani.ac.in>
2024-02-08 08:41:18 +0000

    evalutaion completed with mlflow

--- src/cnnClassifier/components/model_training.py
+++ src/cnnClassifier/components/model_training.py
@@ -11,7 +11,6 @@ class Training:
     def __init__(self, config: TrainingConfig):
         self.config = config
 
-    
     def get_base_model(self):
         self.model = tf.keras.models.load_model(
             self.config.updated_base_model_path
